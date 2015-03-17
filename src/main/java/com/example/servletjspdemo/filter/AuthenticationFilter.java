c6c8f9ffefe1d88d86f41a29ab1f6a23973489f9
Robert Gruberski <rgruberski@gmail.com>
2015-03-17 20:33:16 +0100

    changes

--- src/main/java/com/example/servletjspdemo/filter/AuthenticationFilter.java
+++ src/main/java/com/example/servletjspdemo/filter/AuthenticationFilter.java
@@ -1,7 +1,12 @@
 package com.example.servletjspdemo.filter;
 
+import com.example.servletjspdemo.domain.User;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -19,7 +24,21 @@ public class AuthenticationFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
 
+        HttpSession session = httpRequest.getSession(false);
+        User user = (User) session.getAttribute("user");
+
+        if(user != null) {
+
+            // TODO: Sprawdzanie warunków i odpowiednie przekierowania zgodnie z intrukcją
+            // TODO: przekierowanie za pomocą httpResponse.sendRedirect("/login"); z odpowiednim plikiem
+
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+
+        httpResponse.sendRedirect("/login");
     }
 
     @Override
